7a8508a8120a52542474c46f7266c359d06bb7d8
khalilholberton <1071@holbertonschool.com>
2020-01-05 21:05:40 +0000

    fix betty

--- opcodes.c
+++ opcodes.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * *add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * *add_dnodeint - adds a new node at the beginning of a stack_t.
  * @head: doubly linked list.
  * @n: is int type
  * Return: the address of the new element, or NULL if it failed.
