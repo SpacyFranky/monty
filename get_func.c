1804311e6815572415ca54462905aab629404edb
SpacyFranky <bigaziz@outlook.com>
2020-01-05 20:40:22 +0000

    fixed

--- get_func.c
+++ get_func.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+#include <string.h>
 /**
  * *get_func - this will search for which of the right function to operate
  * opcode command.
@@ -6,14 +7,21 @@
  * @line_number: line number inside the file
  * Return: the needed function.
  */
-void (*get_func(stack_t **stack, unsigned int line_number))(instruction_t)
+void (*get_func(char *word))(stack_t **stack, char *token2,
+			     unsigned int line_number)
 {
 	int i;
 	instruction_t op[] = {
 		{"push", opcode_push},
 		{"pall", opcode_pall},
+		{"pint", opcode_pint},
 		{NULL, NULL}
 	};
 
+	for (i = 0; op[i].opcode != NULL; i++)
+	{
+		if (strcmp(word, op[i].opcode) == 0)
+			return (op[i].f);
+	}
 	return (NULL);
 }
