fe87a329ecb01bb2defa3c86ad57a50cc994f1a2
khalilholberton <1071@holbertonschool.com>
2020-01-05 00:41:41 +0000

    fix add nop more changes

--- opcodes2.c
+++ opcodes2.c
@@ -21,3 +21,33 @@ void opcode_swap(stack_t **stack_t, unsigned int line_number)
 	tempo->next = *stack;
 	*stack = tempo;
 }
+/**
+ *
+ *
+ */
+void upcode_add(stack_t **stack, unsigned int line_number)
+{
+	int total_add;
+        stack_t *tempo;
+
+	(stack == NULL || *stack == NULL ||  (*stack)->next  == NULL)
+        {
+		printf("L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tempo = *stack;
+	(*stack) = (*stack_t)->next;
+	total_add = tempo->n + (*stack)->n;
+	(*stack)->n = total_add;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+/**
+ *
+ *
+ */
+void opcode_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
